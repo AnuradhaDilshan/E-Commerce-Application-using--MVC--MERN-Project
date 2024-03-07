07a4b8c8ec016380a17e9708a75bae7deded5cda
Anuradha Dilshan <101551005+AnuradhaDilshan@users.noreply.github.com>
2024-03-07 19:08:23 +0530

    Backend Changes

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+
+//Configure env
+dotenv.config();
+
+//DATABASE Config
+connectDB();
+
+//REST Object
+const app = express();
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//ROUTES
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+//REST API
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to E-Commerce Application",
+  });
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//RUN Listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
